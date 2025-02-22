b6a629d630770eab996d1b9dded63954a642118e
ricardO <sleiph@gmail.com>
2025-02-22 16:20:39 -0300

    o inicio de um sonho

---
+++ default.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title>Ricardo Alves Gomes</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        
+        <meta charset="utf-8">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+        <meta content="Página padrão" name="description">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Mono:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+        <style>
+            body {
+                margin: 0px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                width: 100vw;
+                height: 100vh;
+                min-height: 675px;
+                background-color: #fcc200;
+                font-family: 'Atkinson Hyperlegible Mono', monospace;
+            }
+            a:hover {
+                cursor: pointer;
+                color: #673DE6;
+                text-decoration: underline;
+            }
+            .content {
+                margin: 5%;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navigation">
+            <img src="/img/eu_16-16.gif" alt="só um boneco de pixelart dançando" />
+        </nav>
+        <div class="content">
+            <h1>Ricardo Alves Gomes</h1>
+            <p>Eu acabei comprando um domínio da Hostinger sem checar se eu podia subir meu projeto de Next.js.</p>
+            <p>Parece que sim, eu posso subir meu super projeto pra hostinger, mas só se eu pagar o melhor plano que eles têm. Eu não quero pagar mais nada além do que eu já paguei, então vou ficar com esse site em php mesmo e só colocar um link pro meu portfolio de verdade.</p>
+            <p><a href="https://ricardo-gomes.vercel.app/home">Aqui meu Portfolio de verdade!</a></p>
+            <p>Não assinem planos sem conferir as funcionalidades liberadas primeiro.</p>
+            
+            <div class="contato">
+                <a href="https://github.com/sleiph"><img src="/img/github.png" alt="logo github" /></a>
+                <a href="https://harpo.itch.io/"><img src="/img/itch.png" alt="logo itch.io" /></a>
+                <a href="https://www.linkedin.com/in/ricardo-gomes-aa40a51b1/"><img src="/img/linkedin.png" alt="logo linkedin" /></a>
+            </div>
+        </div>
+    </body>
+</html>
